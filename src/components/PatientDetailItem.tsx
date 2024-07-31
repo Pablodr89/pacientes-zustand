19f16e7bbff5a585eb99b376ff737ca1c19fd6ef
Pablo <padoro89@gmail.com>
2024-07-31 19:57:21 +0200

    first commit

---
+++ src/components/PatientDetailItem.tsx
@@ -0,0 +1,9 @@
+
+
+export default function PatientDetailItem() {
+    return (
+      <p className="font-bold mb-3 text-gray-700 uppercase">{}: {''}
+          <span className="font-normal normal-case">{}</span>
+      </p>
+    )
+}
