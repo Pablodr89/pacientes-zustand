951afbc7c001dc10d9a2df5eabc2d3088554504f
Pablo <pablodominguez@metlabs.io>
2024-10-29 13:43:57 +0100

    modules and formHook

--- src/components/PatientForm.tsx
+++ src/components/PatientForm.tsx
@@ -1,105 +1,124 @@
-
-
+import { useForm } from "react-hook-form";
+import Error from "./Error";
 
 export default function PatientForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const registerPatient = () => {
+    console.log("nuevoPaciente");
+  };
+
   return (
     <div className="md:w-1/2 lg:w-2/5 mx-5">
-          <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
-  
-          <p className="text-lg mt-5 text-center mb-10">
-              Añade Pacientes y {''}
-              <span className="text-indigo-600 font-bold">Administralos</span>
-          </p>
+      <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
 
-          <form 
-              className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
-              noValidate
-          >
-                <div className="mb-5">
-                    <label htmlFor="name" className="text-sm uppercase font-bold">
-                        Paciente 
-                    </label>
-                    <input  
-                        id="name"
-                        className="w-full p-3  border border-gray-100"  
-                        type="text" 
-                        placeholder="Nombre del Paciente" 
-                    />
+      <p className="text-lg mt-5 text-center mb-10">
+        Añade Pacientes y {""}
+        <span className="text-indigo-600 font-bold">Administralos</span>
+      </p>
 
-                    {/* {errors.name && (
-                        <Error>{errors.name?.message}</Error>
-                    )} */}
-                    
-                </div>
-  
-                <div className="mb-5">
-                    <label htmlFor="caretaker" className="text-sm uppercase font-bold">
-                        Propietario 
-                    </label>
-                    <input  
-                        id="caretaker"
-                        className="w-full p-3  border border-gray-100"  
-                        type="text" 
-                        placeholder="Nombre del Propietario"
-                        />
+      <form
+        className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
+        noValidate
+        onSubmit={handleSubmit(registerPatient)}
+      >
+        <div className="mb-5">
+          <label htmlFor="name" className="text-sm uppercase font-bold">
+            Paciente
+          </label>
+          <input
+            id="name"
+            className="w-full p-3  border border-gray-100"
+            type="text"
+            placeholder="Nombre del Paciente"
+            {...register("name", {
+              required: "El nombre del paciente es obligatorio",
+            })}
+          />
 
-                        {/* {errors.caretaker && (
-                            <Error>{errors.caretaker?.message}</Error>
-                        )} */}
-                </div>
-  
-              <div className="mb-5">
-                <label htmlFor="email" className="text-sm uppercase font-bold">
-                    Email 
-                </label>
-                <input  
-                    id="email"
-                    className="w-full p-3  border border-gray-100"  
-                    type="email" 
-                    placeholder="Email de Registro"
-                />
+          {errors.name && <Error>{errors.name?.message?.toString()}</Error>}
+        </div>
 
-                {/* {errors.email && (
-                    <Error>{errors.email?.message}</Error>
-                )} */}
-              </div>
-  
-              <div className="mb-5">
-                  <label htmlFor="date" className="text-sm uppercase font-bold">
-                      Fecha Alta 
-                  </label>
-                  <input  
-                      id="date"
-                      className="w-full p-3  border border-gray-100"  
-                      type="date"
-                    />
+        <div className="mb-5">
+          <label htmlFor="caretaker" className="text-sm uppercase font-bold">
+            Propietario
+          </label>
+          <input
+            id="caretaker"
+            className="w-full p-3  border border-gray-100"
+            type="text"
+            placeholder="Nombre del Propietario"
+            {...register("caretaker", {
+              required: "El nombre del propietario es obligatorio",
+            })}
+          />
 
-                    {/* {errors.date && (
-                        <Error>{errors.date?.message}</Error>
-                    )} */}
-              </div>
-              
-              <div className="mb-5">
-                  <label htmlFor="symptoms" className="text-sm uppercase font-bold">
-                    Síntomas 
-                  </label>
-                  <textarea  
-                      id="symptoms"
-                      className="w-full p-3  border border-gray-100"  
-                      placeholder="Síntomas del paciente"
-                    />
+          {errors.caretaker && (
+            <Error>{errors.caretaker?.message?.toString()}</Error>
+          )}
+        </div>
 
-                    {/* {errors.symptoms && (
-                        <Error>{errors.symptoms?.message}</Error>
-                    )} */}
-              </div>
-  
-              <input
-                  type="submit"
-                  className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
-                  value='Guardar Paciente'
-              />
-          </form> 
-      </div>
-  )
+        <div className="mb-5">
+          <label htmlFor="email" className="text-sm uppercase font-bold">
+            Email
+          </label>
+          <input
+            id="email"
+            className="w-full p-3  border border-gray-100"
+            type="email"
+            placeholder="Email de Registro"
+            {...register("email", {
+              required: "El email es obligatorio",
+            })}
+          />
+
+          {errors.email && <Error>{errors.email?.message?.toString()}</Error>}
+        </div>
+
+        <div className="mb-5">
+          <label htmlFor="date" className="text-sm uppercase font-bold">
+            Fecha Alta
+          </label>
+          <input
+            id="date"
+            className="w-full p-3  border border-gray-100"
+            type="date"
+            {...register("date", {
+              required: "La fecha de alta es obligatoria",
+            })}
+          />
+
+          {errors.date && <Error>{errors.date?.message?.toString()}</Error>}
+        </div>
+
+        <div className="mb-5">
+          <label htmlFor="symptoms" className="text-sm uppercase font-bold">
+            Síntomas
+          </label>
+          <textarea
+            id="symptoms"
+            className="w-full p-3  border border-gray-100"
+            placeholder="Síntomas del paciente"
+            {...register("symptoms", {
+              required: "Los síntomas del paciente es obligatorio",
+            })}
+          />
+
+          {errors.symptoms && (
+            <Error>{errors.symptoms?.message?.toString()}</Error>
+          )}
+        </div>
+
+        <input
+          type="submit"
+          className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
+          value="Guardar Paciente"
+        />
+      </form>
+    </div>
+  );
 }
